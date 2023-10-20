52e2b823c50f4f4153026362316adf60927b73f3
Zeynep Toy <zeynep.toy@btpro.net>
2023-10-21 00:00:54 +0300

    Add useCases and endPoint

---
+++ MovieTip/MTNetwork/MovieEndPoint/MovieDetailsUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  MovieDetailsUseCase.swift
+//  MTNetwork
+//
+//  Created by Saniye Toy on 20.10.2023.
+//
+
+import Foundation
+import Alamofire
+
+
+protocol MovieDetailsUseCaseType {
+    func getMovieDetails(params: [String: Any], completion: @escaping((Result<MovieDetailsResponse, Error>)) -> Void)
+}
+
+
+struct MovieDetailsUseCase: MovieDetailsUseCaseType {
+    
+    public init() {
+        
+    }
+    
+    func getMovieDetails(params: [String : Any], completion: @escaping((Result<MovieDetailsResponse, Error>)) -> Void) {
+        return BaseService.request( MovieEndPoint.movieDetails(params: params), completion: completion)
+    }
+}
