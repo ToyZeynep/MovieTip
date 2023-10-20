500654431a02223b7ec119c77336d2cb7d1233d1
Zeynep Toy <zeynep.toy@btpro.net>
2023-10-20 23:08:38 +0300

    Create local frameworks

---
+++ MovieTip/MTComponents/MTComponents.h
@@ -0,0 +1,18 @@
+//
+//  MTComponents.h
+//  MTComponents
+//
+//  Created by Saniye Toy on 20.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MTComponents.
+FOUNDATION_EXPORT double MTComponentsVersionNumber;
+
+//! Project version string for MTComponents.
+FOUNDATION_EXPORT const unsigned char MTComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTComponents/PublicHeader.h>
+
+
