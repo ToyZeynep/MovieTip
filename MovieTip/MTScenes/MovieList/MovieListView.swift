34dbd9801238253a65bb1791d4feee7ca44f5d02
Zeynep Toy <zeynep.toy@btpro.net>
2023-10-21 13:00:53 +0300

    create files

---
+++ MovieTip/MTScenes/MovieList/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MovieTip
+//
+//  Created by Saniye Toy on 21.10.2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ MovieTip/MTScenes/MovieList/MovieListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieListViewModel.swift
+//  MovieTip
+//
+//  Created by Saniye Toy on 21.10.2023.
+//
+
+import Foundation
