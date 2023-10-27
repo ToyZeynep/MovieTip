bf0b1039968c0cb27c4b127a9f12e588e28a32f0
Zeynep Toy <zeynep.toy@btpro.net>
2023-10-28 00:18:51 +0300

    add search bar, filter and sort buttons

--- MovieTip/MTScenes/MovieList/MovieListView.swift
+++ MovieTip/MTScenes/MovieList/MovieListView.swift
@@ -33,30 +33,74 @@ struct MovieCard: View {
     }
 }
 
+struct CustomSearchBar: View {
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+            
+            TextField("Search movies...", text: $searchText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.vertical, 8)
+        }
+        .padding(.horizontal)
+    }
+}
+
 struct MovieListView: View {
     @ObservedObject var movieListViewModel: MovieListViewModel
     @State var type: String = "movie"
+    @State private var searchText = ""
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], alignment: .center, spacing: 10) {
-                    if let movieList = movieListViewModel.movieListResponse?.movies {
-                        
-                        ForEach(0..<(movieList.count), id: \.self) { index in
-                            let movie = movieList[index]
-                            MovieCard(movie: movie)
+            VStack {
+                HStack(spacing: 1) {
+                    CustomSearchBar(searchText: $searchText)
+                    
+                    Button(action: {
+                      
+                    }) {
+                        Image(systemName: "slider.horizontal.3")
+                            .padding(.horizontal, 2)
+                            .foregroundColor(.gray)
+                    }
+                    Button(action: {
+                      
+                    }) {
+                        Image(systemName: "arrow.up.arrow.down.square.fill")
+                            .padding(.horizontal, 2)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.horizontal, 2)
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], alignment: .center, spacing: 10) {
+                        if let movieList = movieListViewModel.movieListResponse?.movies {
+                            ForEach(0..<(movieList.count), id: \.self) { index in
+                                let movie = movieList[index]
+                                MovieCard(movie: movie)
+                            }
                         }
                     }
                 }
-            }
-            .navigationTitle(type)
-            .onAppear(){
-                movieListViewModel.getMovieList(searchText: "Ali", page: 1 , type: type)
+                
+                .onChange(of: searchText) { newValue in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        movieListViewModel.getMovieList(searchText: newValue, page: 1 , type: type)
+                    }
+                }
+                .onAppear(){
+                    movieListViewModel.getMovieList(searchText: "ara", page: 1 , type: type)
+                }
             }
         }
+        .navigationBarBackButtonHidden(true)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
